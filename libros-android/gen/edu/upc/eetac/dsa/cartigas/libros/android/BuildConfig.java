dc2c39620098e7e6085decd185a4e8315039ad12
Mohamed <filatan@gmail.com>
2014-05-23 10:19:52 +0200

    first commit

---
+++ libros-android/gen/edu/upc/eetac/dsa/cartigas/libros/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.upc.eetac.dsa.cartigas.libros.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
